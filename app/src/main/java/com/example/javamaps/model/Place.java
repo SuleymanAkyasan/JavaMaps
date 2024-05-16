221c97046f6f94b88ef0d92526e805f117dc34e2
Suleyman Akyasan <vipsuleyman6134@gmail.com>
2024-05-16 11:55:40 +0300

    added to github

---
+++ app/src/main/java/com/example/javamaps/model/Place.java
@@ -0,0 +1,33 @@
+package com.example.javamaps.model;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity
+public class Place implements Serializable {
+
+    //şu Entity falanı eklemeliyiz yoksa room database sınıfını anlayamayız
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "name")
+    public String name;
+
+    @ColumnInfo(name = "latitude")
+    public Double latitude;
+
+    @ColumnInfo(name = "longitude")
+    public Double longitude;
+
+    public Place(String name , Double latitude, Double longitude){
+        //id istemedik çünkü otomatik oluşturulacak...
+        this.name=name;
+        this.latitude=latitude;
+        this.longitude=longitude;
+    }
+
+
+}
